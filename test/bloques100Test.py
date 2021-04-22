4fdd61b0353e06b2ca789687bf58350909fd2024
Stefano Simoni <stefano.simoni.10.xg@gmail.com>
2021-04-22 12:10:04 -0300

    Commit 11
    Class Blockchain:
    Se agrego metodo para validar en la Blockchain si cada uno de los objetos creados tiene el hash de su anterior
    Se agrego metodo para traer el ultimo bloque de la Blockchain
    Nuevos metodos agregados comentados
    
    Test blockchainTest:
    Ahora el test del bloque 2 dara un hash un solo 0 para ver como es el hash cuando el dia es impar
    Se agregaron test de los dos nuevos metodos creados en la class Blockchain

--- test/bloques100Test.py
+++ test/bloques100Test.py
@@ -6,15 +6,20 @@ sys.path.append(ruta)
 from src.blockchain import Blockchain
 from src.bloque import Bloque
 
+test = Blockchain()
+for i in range(100):
+    test._Blockchain__crearBloque("alguien@gmail.com", "test", "hashArch", "2021-04-12 13:00:00")
+
 class Bloques100Test(unittest.TestCase):
     def test_100Bloques_should_to_be_true_when_hashAnt_and_hashBlq_be_equals_and_bloque_and_blockchainPos50_hashs_be_equals(self):
-        test = Blockchain()
-        for i in range(100):
-            test._Blockchain__crearBloque("alguien@gmail.com", "test", "hashArch", "2021-04-12 13:00:00")
         bloque49 = test.getBloquePorId(49)
         bloque50 = test.getBloquePorId(50)
         self.assertEqual(bloque50.hashAnt, bloque49.hashBlq)
         bloque = Bloque(50, "alguien@gmail.com", "test", "hashArch", "2021-04-12 13:00:00", bloque49.hashBlq, 0)
         self.assertEqual(bloque.hashBlq, bloque50.hashBlq)
+
+    def test_100Bloques_should_to_be_true_when_hashAnt_and_hashBlq_be_equals_and_bloque_and_blockchainPos50_hashs_be_equals(self):
+        esValida = test.validar()
+        self.assertEqual(True, esValida)
 
 if __name__ == '__main__': unittest.main()
